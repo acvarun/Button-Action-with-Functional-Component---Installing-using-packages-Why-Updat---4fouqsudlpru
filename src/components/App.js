784198933e311c927a751ac70bec5903ef92de8e
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-15 22:36:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-mport React, {Component, useState} from "react";
+import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 
@@ -9,6 +9,7 @@ function App() {
 }
   return (
     <div id="main">
+      // Do not alter the main div
       <button id="click" onClick={paragraph}>Click</button>
       {
         Para && (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
