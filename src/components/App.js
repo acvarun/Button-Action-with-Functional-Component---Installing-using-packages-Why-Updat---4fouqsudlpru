3f94f1cba15ad41a2b2a0bc6c70edba33ae648c0
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-15 22:30:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React, {Component, useState} from "react";
+mport React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
 function App() {
+  const [Para, setPara] = useState(false);
+  const paragraph = () => {
+  setPara(true)
+}
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={paragraph}>Click</button>
+      {
+        Para && (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+      }
     </div>
   );
 }
